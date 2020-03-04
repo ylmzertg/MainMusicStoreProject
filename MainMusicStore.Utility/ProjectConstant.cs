cfafa141b1880e78420d882d556083ac526d5159
ertugrul yilmaz <ylmzertg@gmail.com>
2020-03-04 21:32:46 +0300

    N-Tier degısıklıkler ve gelıstırmeler proje alşt yapısı ıcın

---
+++ MainMusicStore.Utility/ProjectConstant.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MainMusicStore.Utility
+{
+    public static class ProjectConstant
+    {
+        public const string ResultNotFound = "Data Not Found";
+    }
+}
