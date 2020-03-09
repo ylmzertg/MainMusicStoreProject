987668210b72a60d7892f2c39c4ce03f8d453acb
ertugrul yilmaz <ylmzertg@gmail.com>
2020-03-09 21:37:06 +0300

    Category Crud Done

--- MainMusicStore/Areas/Admin/Controllers/CategoryController.cs
+++ MainMusicStore/Areas/Admin/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using MainMusicStore.DataAccess.IMainRepository;
+using MainMusicStore.Models.DbModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MainMusicStore.Areas.Admin.Controllers
@@ -7,21 +8,21 @@ namespace MainMusicStore.Areas.Admin.Controllers
     public class CategoryController : Controller
     {
         #region Variables
-        private readonly IUnitOfWork _uow; 
+        private readonly IUnitOfWork _uow;
         #endregion
 
         #region CTOR
         public CategoryController(IUnitOfWork uow)
         {
             _uow = uow;
-        } 
+        }
         #endregion
 
         #region Actions
         public IActionResult Index()
         {
             return View();
-        } 
+        }
         #endregion
 
         #region API CALLS
@@ -29,7 +30,66 @@ namespace MainMusicStore.Areas.Admin.Controllers
         {
             var allObj = _uow.category.GetAll();
             return Json(new { data = allObj });
-        } 
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var deleteData = _uow.category.Get(id);
+            if (deleteData == null)
+                return Json(new { success = false,message ="Data Not Found!"});
+
+            _uow.category.Remove(deleteData);
+            _uow.Save();
+            return Json(new { success = true ,message ="Delete Operation Successfully"});
+        }
+
         #endregion
+
+        /// <summary>
+        /// Create Or Update Get Method
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        /// 
+        [HttpGet]
+        public IActionResult Upsert(int? id)
+        {
+            Category cat = new Category();
+            if (id == null)
+            {
+                //This for Create
+                return View(cat);
+            }
+
+            cat = _uow.category.Get((int)id);
+            if (cat != null)
+            {
+                return View(cat);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (category.Id == 0)
+                {
+                    //Create
+                    _uow.category.Add(category);
+                }
+                else
+                {
+                    //Update
+                    _uow.category.Update(category);
+                }
+                _uow.Save();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
     }
 }
--- MainMusicStore/wwwroot/js/category.js
+++ MainMusicStore/wwwroot/js/category.js
@@ -29,3 +29,31 @@ function loadDataTable() {
         ]
     });
 }
+
+
+
+function Delete(url) {
+    swal({
+        title: 'Are you sure you want to Delete?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        buttons: true,
+        dangerMode:true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                type: "DELETE",
+                url: url,
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+                    }
+                    else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
