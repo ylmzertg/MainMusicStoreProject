a17acdaf80029e5a259c6b86a691313bbef6bb1f
ertugrul yilmaz <ylmzertg@gmail.com>
2020-03-15 15:07:35 +0300

    User managment Section Done

---
+++ MainMusicStore/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,73 @@
+﻿using MainMusicStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace MainMusicStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        #region Variables
+        //private readonly IUnitOfWork _uow;
+        private readonly ApplicationDbContext _db;
+        #endregion
+
+        #region CTOR
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        #region Actions
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+
+        #region API CALLS
+        public IActionResult GetAll()
+        {
+            var userList = _db.ApplicationUsers.Include(c => c.Company).ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+
+            foreach (var user in userList)
+            {
+                var roleId = userRole.FirstOrDefault(u => u.UserId == user.Id).RoleId;
+                user.Role = roles.FirstOrDefault(u => u.Id == roleId).Name;
+
+                if (user.Company == null)
+                {
+                    user.Company = new Models.DbModels.Company()
+                    {
+                        Name = string.Empty
+                    };
+                }
+            }
+            return Json(new { data = userList });
+        }
+
+        [HttpPost]
+        public IActionResult LockUnlock([FromBody] string id)
+        {
+            var data = _db.ApplicationUsers.FirstOrDefault(u => u.Id == id);
+            if (data == null)
+                return Json(new { success = false, message = "Error while locking/unlocking" });
+
+            if (data.LockoutEnd != null && data.LockoutEnd > DateTime.Now)
+                data.LockoutEnd = DateTime.Now;
+            else
+                data.LockoutEnd = DateTime.Now.AddYears(10);
+
+            _db.SaveChanges();
+            return Json(new { success = true, message = "Operation Successfully" });
+        }
+
+        #endregion
+
+    }
+}
---
+++ MainMusicStore/wwwroot/js/user.js
@@ -0,0 +1,69 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/User/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "email", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "role", "width": "15%" },
+            {
+                "data":
+                {
+                    id: "id", lockoutEnd: "lockoutEnd"
+                },
+                "render": function (data) {
+
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+                    if (lockout > today) {
+                        //user ,s currently locked
+                        return `
+                            <div class="text-center">
+                                <a onclick=LockUnlock('${data.id}') class="btn btn-danger text-white" style="cursor:pointer">
+                                    <i class="fas fa-lock-open"></i>Unlock 
+                                </a>
+                            </div>
+                           `;
+                    } else {
+                        return `
+                            <div class="text-center">                              
+                                <a onclick=LockUnlock('${data.id}') class="btn btn-success text-white" style="cursor:pointer">
+                                    <i class="fas fa-lock"></i>Lock 
+                                </a>
+                            </div>
+                           `;
+                    }
+
+                }, "width": "25%"
+            }
+        ]
+    });
+}
+
+
+
+function LockUnlock(id) {
+    $.ajax({
+        type: "POST",
+        url: '/Admin/User/LockUnlock',
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                dataTable.ajax.reload();
+            }
+            else {
+                toastr.error(data.message);
+            }
+        }
+    });
+}
